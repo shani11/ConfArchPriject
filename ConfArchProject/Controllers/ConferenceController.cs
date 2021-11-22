896198c6bc68196ca9ef9b8fda76852d2c279169
Shani Kumar <shani.kumar@globallogic.com>
2021-11-22 18:26:38 +0530

    initial commit

---
+++ ConfArchProject/Controllers/ConferenceController.cs
@@ -0,0 +1,39 @@
+﻿using ConfArch.Data.Models;
+using ConfArch.Data.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ConfArch.Web.Controllers
+{
+    [AllowAnonymous]
+    public class ConferenceController: Controller
+    {
+        private readonly IConferenceRepository repo;
+
+        public ConferenceController(IConferenceRepository repo)
+        {
+            this.repo = repo;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Title = "Organizer - Conference Overview";
+            return View(await repo.GetAll());
+        }
+
+        public IActionResult Add()
+        {
+            ViewBag.Title = "Organizer - Add Conference";
+            return View(new ConferenceModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ConferenceModel model)
+        {
+            if (ModelState.IsValid)
+                await repo.Add(model);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ConfArchProject/Startup.cs
@@ -0,0 +1,68 @@
+using ConfArch.Data;
+using ConfArch.Data.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfArchProject
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddScoped<IConferenceRepository, ConferenceRepository>();
+            services.AddScoped<IProposalRepository, ProposalRepository>();
+            services.AddScoped<IAttendeeRepository, AttendeeRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            services.AddDbContext<ConfArchDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                    assembly => assembly.MigrationsAssembly(typeof(ConfArchDbContext).Assembly.FullName)));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Conference}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
